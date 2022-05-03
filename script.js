91cf4716c172879caf09d478456f2edf89b9a835
Kátia Miglioli <kt.miglioli@gmail.com>
2022-05-03 17:43:12 -0300

    Add: validação para e-mail invalido

---
+++ script.js
@@ -0,0 +1,6 @@
+const ValidateEmail = (input) => {
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (!input.value.match(validRegex)) {
+        alert("E-mail inválido!");
+      }
+}
